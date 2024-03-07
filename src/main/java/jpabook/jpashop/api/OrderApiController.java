32ecc88a4154f8a9fa6412ad3a44ccf19a23a9b4
승현 <ghkrsla@gmail.com>
2024-03-07 22:39:21 +0900

    jpq에서 dto 컬렉션 직접 조회 최적화 전

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,13 +6,13 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,9 +22,10 @@ import java.util.List;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("api/v1/orders")
-    public List<Order> orderV1() {
+    public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName();
@@ -36,7 +37,7 @@ public class OrderApiController {
     }//쓰지말아야 할 방법 바로 entity 반환하기 떄문
 
     @GetMapping("api/v2/orders")
-    public List<OrderDto> orderV2() {
+    public List<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> collect = orders.stream()
             .map(OrderDto::new)
@@ -68,7 +69,7 @@ public class OrderApiController {
     }//쓰지말아야함 쿼리 너무 많이 나감
 
     @GetMapping("api/v3/orders")
-    public List<OrderDto> orderV3() {
+    public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
 //        for (Order order : orders) {
 //            System.out.println("order ref = "+order + "id = " + order.getId());
@@ -81,7 +82,7 @@ public class OrderApiController {
     }//페이징 불가
 
     @GetMapping("api/v3.1/orders")
-    public List<OrderDto> orderV3_page(
+    public List<OrderDto> ordersV3_page(
         @RequestParam(value = "offset", defaultValue = "0") int offset,
         @RequestParam(value = "limit", defaultValue = "100") int limit
     ) {
@@ -94,16 +95,22 @@ public class OrderApiController {
         return result;
     }
 
-    @Getter
-    static class OrderItemDto {
-        private String itemName; //상품 명
-        private int orderPrice; //주문 가격
-        private int count;  //주문 수량
+    @GetMapping("api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDto();
+    }
 
-        public OrderItemDto(OrderItem orderItem) {
-            itemName = orderItem.getItem().getName();
-            orderPrice = orderItem.getOrderPrice();
-            count = orderItem.getCount();
+
+@Getter
+static class OrderItemDto {
+    private String itemName; //상품 명
+    private int orderPrice; //주문 가격
+    private int count;  //주문 수량
+
+    public OrderItemDto(OrderItem orderItem) {
+        itemName = orderItem.getItem().getName();
+        orderPrice = orderItem.getOrderPrice();
+        count = orderItem.getCount();
 
         }
     }
---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.repository.order.query;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+@Data
+public class OrderItemQueryDto {
+
+    @JsonIgnore
+    private Long orderId;
+
+    public OrderItemQueryDto(Long orderId, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+}
